b4d33bead73ad6a8ef4633fbc088df1315684864
MJ Rios <45809332+VirusFusion@users.noreply.github.com>
2021-06-02 17:22:43 -0700

    Add files via upload

---
+++ rust_program/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    let mut E = 69; // E can not be changed
+
+    println!("the value of E is {}", your mom);
+
+    E = 420; //changing the value of E
+
+    println!("the value of E is {}", your mom);
+}
